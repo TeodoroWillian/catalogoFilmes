a94af765fef66501dcf91994e4f4f85a72ffd951
TeodoroWillian <totawillian@gmail.com>
2022-06-12 12:24:42 -0400

    Projeto Estruturado

---
+++ src/models/Filmes.js
@@ -0,0 +1,35 @@
+const {Sequelize} = require("sequelize");
+
+const database = require("../database/db");
+
+const Filme = database.sequelize.define(
+    "filme",
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        nome:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        descricao:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        imagem:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    },
+        {
+            freezeTableName: true,
+            timestamps:false,
+            createdAT: false,
+            updateAt: false,
+        }
+);
+
+module.exports = Filme;
---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+const routes = require("express").Router();
+
+const FilmeController = require("../controllers/FilmeControllers");
+
+routes.get("/",FilmeController.getAll)
+
+
+module.exports = routes;
